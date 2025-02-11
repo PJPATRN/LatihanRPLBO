b85b0cf817212d800f5760b5903a37475fb0355a
PJPATRN <143705835+PJPATRN@users.noreply.github.com>
2025-02-11 13:27:57 +0700

    Perubahan_hari_selasa

--- Japaskrip.js
+++ Japaskrip.js
@@ -1,16 +1,22 @@
 const namaSaya = "Paulus Ungirwalu";
 let Umur = 21;
 
+const biodata = document.getElementById('biodata');
+console.log(biodata);
+
 function generateBiodata() {
-    console.log(`Nama saya adalah ${namaSaya} dan usia saya ${Umur} tahun`);
+    console.log(`Nama saya adalah ${namaSaya} dan usia saya ${Umur} tahun`);   
 
+    let generasi;
 
-    if (Umur < 25 && Umur > 10){
-        console.log("Anda Remaja");
+    if (Umur < 18 && Umur > 10){
+        generasi = "gen Alpha";
+    } else if (Umur > 18 && Umur < 25) {
+        generasi = "gen Z";
     } else {
-        console.log("Unavailable");
+        generasi = "gen apa bjir";
     }
-
+    return console.log(`Generasi anda adalah : ${generasi}`);
 }
 
 generateBiodata()
